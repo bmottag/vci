9563b4eaf88e14ea535e0b9c4bc2b630e8467611
bmottag <benmotta@gmail.com>
2026-03-21 08:20:46 -0400

    Basecontroller to manage menu and session

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -20,6 +20,10 @@ use Psr\Log\LoggerInterface;
  */
 abstract class BaseController extends Controller
 {
+
+    protected $session;
+    protected $menu;
+
     /**
      * Be sure to declare properties for any property fetch you initialized.
      * The creation of dynamic property is deprecated in PHP 8.2.
@@ -30,16 +34,126 @@ abstract class BaseController extends Controller
     /**
      * @return void
      */
-    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    public function initController($request, $response, $logger)
     {
-        // Load here all helpers you want to be available in your controllers that extend BaseController.
-        // Caution: Do not put the this below the parent::initController() call below.
-        // $this->helpers = ['form', 'url'];
-
-        // Caution: Do not edit this line.
         parent::initController($request, $response, $logger);
 
-        // Preload any models, libraries, etc, here.
-        // $this->session = service('session');
+        $this->session = session();
+
+        // 🔥 CARGAR MENÚ UNA SOLA VEZ
+        $this->menu = $this->prepareMenu();
+    }
+
+    /**
+     * Prepara los datos del menú
+     */
+    protected function prepareMenu()
+    {
+        $userRol = $this->session->get('rol');
+
+        $menuData = [
+            'leftMenu' => [],
+            'topMenu' => []
+        ];
+
+        // Left Menu
+        $itemsLeftMenu = $this->generalModel->get_role_menu([
+            'idRole' => $userRol,
+            'menuType' => 1,
+            'menuState' => 1
+        ]);
+
+		$menuIndex = [];
+
+		if ($itemsLeftMenu) {
+			foreach ($itemsLeftMenu as $item) {
+
+				$menuId = $item['fk_id_menu'];
+
+				// 🔹 Si el menú NO existe aún → lo creamos
+				if (!isset($menuIndex[$menuId])) {
+
+					$links = [];
+
+					if (!$item['menu_url']) {
+						$links = $this->generalModel->get_role_access([
+							'idRole' => $userRol,
+							'idMenu' => $menuId,
+							'linkState' => 1,
+							'menuType' => 1
+						]);
+					}
+
+					$menuIndex[$menuId] = [
+						'name' => $item['menu_name'],
+						'icon' => $item['menu_icon'],
+						'url'  => $item['menu_url'] ? base_url($item['menu_url']) : null,
+						'links'=> $links
+					];
+				}
+
+			}
+		}
+
+		$menuData['leftMenu'] = array_values($menuIndex);
+
+		// Top Menu
+		$itemsTopMenu = $this->generalModel->get_role_menu([
+			'idRole' => $userRol,
+			'menuType' => 2,
+			'menuState' => 1
+		]);
+
+		$topIndex = [];
+
+		if ($itemsTopMenu) {
+			foreach ($itemsTopMenu as $item) {
+
+				$menuId = $item['fk_id_menu'];
+
+				if (!isset($topIndex[$menuId])) {
+
+					$links = [];
+
+					if (!$item['menu_url']) {
+						$links = $this->generalModel->get_role_access([
+							'idRole' => $userRol,
+							'idMenu' => $menuId,
+							'linkState' => 1,
+							'menuType' => 2
+						]);
+					}
+
+					$topIndex[$menuId] = [
+						'name'  => $item['menu_name'],
+						'icon'  => $item['menu_icon'],
+						'url'   => $item['menu_url'] ? base_url($item['menu_url']) : null,
+						'links' => $links
+					];
+				}
+			}
+		}
+
+		$menuData['topMenu'] = array_values($topIndex);
+
+        return $menuData;
+    }
+
+    // 🔥 Helper para renderizar vistas
+    protected function render($view, $data = [])
+    {
+        $data['leftMenu'] = $this->menu['leftMenu'];
+        $data['topMenu']  = $this->menu['topMenu'];
+        $data['view']     = $view;
+
+        return view('layout', $data);
+    }
+
+    protected function renderTopOnly($view, $data = [])
+    {
+        $data['topMenu']  = $this->menu['topMenu'];
+        $data['view']     = $view;
+
+        return view('layout_top_only', $data);
     }
 }
