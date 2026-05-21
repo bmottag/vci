0e35cd5372327c1e8fa222669463f40e8105d150
bmottag <benmotta@gmail.com>
2026-05-21 16:37:24 -0400

    Dashboard: Generator records adjustment

--- app/Models/GeneralModel.php
+++ app/Models/GeneralModel.php
@@ -470,6 +470,7 @@ class GeneralModel extends Model
 		}
 
 		$builder->orderBy('I.date_issue', 'desc');
+		$builder->limit($arrData["limit"]);
 		$query = $builder->get();
 
         $result = $query->getResultArray();
