8d86dc1fa59338c976fabd9a32af65379b75c3eb
bmottag <benmotta@gmail.com>
2026-09-09 13:33:04 -0400

    JHA PDF adjustment to scape caracters

--- app/Views/jobs/reporte_jha_pdf.php
+++ app/Views/jobs/reporte_jha_pdf.php
@@ -1,4 +1,8 @@
 <?php
+	// escape user-entered text so it can't break the HTML/table structure TCPDF parses
+	$esc = fn($value) => htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+	$escNl = fn($value) => nl2br($esc($value));
+
 	// create some HTML content
 	$html = '<br><h2 align="center" style="color:#337ab7;">JOB HAZARDS ANALYSIS<br><br></h2>
 				<style>
@@ -17,19 +21,19 @@
 			<table border="0" cellspacing="0" cellpadding="5">
 				<tr>
 					<th bgcolor="#337ab7" style="color:white;"><strong>Job Code/Name: </strong></th>
-					<th colspan="3">' . strtoupper($info[0]['job_description']). '</th>
+					<th colspan="3">' . $esc(strtoupper($info[0]['job_description'])). '</th>
 				</tr>
-			
+
 				<tr>
 					<th bgcolor="#337ab7" style="color:white;"><strong>Done by: </strong></th>
-					<th>' . $info[0]['name']. '</th>
+					<th>' . $esc($info[0]['name']). '</th>
 					<th bgcolor="#337ab7" style="color:white;"><strong>Date & Time: </strong></th>
-					<th>' . $info[0]['date_log']. '</th>
+					<th>' . $esc($info[0]['date_log']). '</th>
 				</tr>
-			
+
 				<tr>
 					<th bgcolor="#337ab7" style="color:white;"><strong>Observation: </strong></th>
-					<th colspan="3">' . $info[0]['observation']. '</th>
+					<th colspan="3">' . $escNl($info[0]['observation']). '</th>
 				</tr>
 
 			</table>';
@@ -55,13 +59,13 @@
 						$i = 0;
 						foreach ($hazards as $data):
 							$i++;
-							$html.= '<tr>';					
+							$html.= '<tr>';
 							$html.= '<th align="center">' . $i . '</th>';
-							$html .= '<th >' . $data['hazard_activity'] . '</th>';
-							$html.= '<th >' . $data['hazard_description'] . '</th>';					
+							$html .= '<th >' . $escNl($data['hazard_activity']) . '</th>';
+							$html.= '<th >' . $escNl($data['hazard_description']) . '</th>';
 							$priority = $data['priority_description']==""?"-":$data['priority_description'];
-							$html.= '<th align="center">' . $priority . '</th>';
-							$html.= '<th >' . $data['solution']  . '</th>';
+							$html.= '<th align="center">' . $esc($priority) . '</th>';
+							$html.= '<th >' . $escNl($data['solution'])  . '</th>';
 							$html.= '</tr>';
 						endforeach;
 					}
